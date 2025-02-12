1bd5b17dc3a2ac6b037a9595bc3392929d456499
michaelclemens <michaelclemens8@hotmail.com>
2025-02-12 15:31:56 +0000

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), checker({ typescript: true, eslint: { lintCommand: 'eslint .', useFlatConfig: true } })],
+})
