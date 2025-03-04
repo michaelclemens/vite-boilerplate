ab021877fae5e344b95fa15817dc3c0d37e46ccf
Michael Clemens <85890874+michaelclemens@users.noreply.github.com>
2025-03-04 15:48:12 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), checker({ typescript: true, eslint: { lintCommand: 'eslint .', useFlatConfig: true } })],
+  plugins: [react(), tailwindcss(), checker({ typescript: true, eslint: { lintCommand: 'eslint src', useFlatConfig: true } })],
 })
